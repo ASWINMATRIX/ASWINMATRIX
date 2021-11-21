d2561913de584c3548b873442eae76644c16fd71
Aswin kkd <87300673+ASWINMATRIX@users.noreply.github.com>
2021-11-21 22:59:34 +0530

    Update contact.js

--- plugins/contact.js
+++ plugins/contact.js
@@ -9,8 +9,8 @@ Ktb.addCommand({ pattern: 'owner ?(.*)', fromMe: false, desc: 'owner number' }, 
 //coded by saidali
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:PrinceRudh [OWNER]\n' // full name
-            + 'ORG:PrinceRudh;\n' // 
+            + 'FN:EvaMwol [OWNER]\n' // full name
+            + 'ORG:Aswin;\n' // 
             + 'TEL;type=CELL;type=VOICE;waid=917736598606:+917736598606\n' // WhatsApp ID + phone number
             + 'END:VCARD'
 await message.client.sendMessage(message.jid,{displayname: "EvaMwol [OWNER]", vcard: vcard}, MessageType.contact)
